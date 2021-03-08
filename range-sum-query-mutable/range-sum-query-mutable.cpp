be3fdcf61fa09cf188927e4329bbf4990bc0f3fa
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2021-03-08 20:30:56 +0530

    Time: 376 ms (48.91%), Space: 151.1 MB (28.68%) - LeetHub

--- range-sum-query-mutable/range-sum-query-mutable.cpp
+++ range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,50 +1,77 @@
 class NumArray {
 public:
+    int *tree;
     int n;
-    int *arr;
-    int *fenwickTree;
     
-    void updateFenwickTree(int index,int val)
+    void buildTree(vector<int> &nums,int index,int start,int end)
     {
-        for( ; index <= n; index += (index & -index))
+        if(start > end) return;
+        
+        if(start == end)
         {
-            fenwickTree[index] += val;
+            tree[index] = nums[start];
+            return;
         }
+        
+        int mid = (start + end)/2;
+        
+        buildTree(nums,2*index,start,mid);
+        
+        buildTree(nums,2*index + 1,mid+1,end);
+        
+        tree[index] = tree[2*index] + tree[2*index + 1];
     }
     
     NumArray(vector<int>& nums) {
         n = nums.size();
         
-        arr = new int[n+1]();
-        fenwickTree = new int[n+1]();
+        tree = new int[4*n + 1];
+        buildTree(nums,1,0,n-1);
+    }
+    
+    void updateTree(int index,int start,int end,int i,int val)
+    {
+        if(i < start or i > end) return;
         
-        for(int i=0;i<n;i++) arr[i+1] = nums[i];
+        if(start == end)
+        {
+            tree[index] = val;
+            return;
+        }
         
-        for(int i=1;i<=n;i++) updateFenwickTree(i,arr[i]);
+        int mid = (start + end)/2;
+        
+        updateTree(2*index,start,mid,i,val);
+        
+        updateTree(2*index + 1,mid+1,end,i,val);
+        
+        tree[index] = tree[2*index] + tree[2*index + 1];
     }
     
     void update(int index, int val) {
-        index++; //for 1 based indexing
-        
-        updateFenwickTree(index,val - arr[index]);
-        arr[index] = val;
+        updateTree(1,0,n-1,index,val);
     }
     
-    int query(int index)
+    int query(int index,int start,int end,int qs,int qe)
     {
-        int ans = 0;
-        for( ; index > 0; index -= (index & -index))
+        if(qe < start or qs > end) return 0;
+        
+        if(qs <= start and qe >= end) 
         {
-            ans += fenwickTree[index];
+            return tree[index];
         }
         
-        return ans;
+        int mid = (start + end)/2;
+        
+        int leftAns = query(2*index,start,mid,qs,qe);
+        
+        int rightAns = query(2*index + 1,mid+1,end,qs,qe);
+        
+        return leftAns + rightAns;
     }
     
     int sumRange(int left, int right) {
-        left++; right++;
-        
-        return query(right) - query(left - 1);
+        return query(1,0,n-1,left,right);
     }
 };
 
