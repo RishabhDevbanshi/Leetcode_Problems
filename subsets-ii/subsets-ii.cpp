2c78ea8ac146a28f790f942d330363e9ab02a151
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2021-06-29 11:53:05 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (86.48%) - LeetHub

--- subsets-ii/subsets-ii.cpp
+++ subsets-ii/subsets-ii.cpp
@@ -1,23 +1,20 @@
 class Solution {
-    void recur(vector<int> &nums,int n,int i,vector<int> arr,vector<vector<int>> &res)
+    void helper(int idx,vector<int> &nums,vector<int> &arr,vector<vector<int>> &res)
     {
-        if(i >= n)
+        res.push_back(arr);
+        for(int i=idx;i<nums.size();i++)
         {
-            if(find(res.begin(),res.end(),arr) == res.end()) res.push_back(arr);
-            return;
+            if(i != idx and nums[i] == nums[i-1]) continue;
+            arr.push_back(nums[i]);
+            helper(i+1,nums,arr,res);
+            arr.pop_back();
         }
-        
-        recur(nums,n,i+1,arr,res);
-        arr.push_back(nums[i]);
-        recur(nums,n,i+1,arr,res);
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> arr;
+        vector<int> arr; vector<vector<int>> res;
         sort(nums.begin(),nums.end());
-        
-        recur(nums,nums.size(),0,arr,res);
+        helper(0,nums,arr,res);
         
         return res;
     }
