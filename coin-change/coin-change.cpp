7e77ab6ba828c2ca4485039e39601bf4654d0448
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2021-07-16 11:47:05 +0530

    Time: 112 ms (31.33%), Space: 49.6 MB (6.65%) - LeetHub

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,25 +1,23 @@
 class Solution {
+    int recur(vector<int> &coins,int tar,int n,vector<vector<int>> &dp)
+    {
+        if(tar == 0) return dp[n][tar] = 0;
+        if(n == 0) return dp[n][tar] = INT_MAX - 3;
+        
+        if(dp[n][tar] != -1)
+                return dp[n][tar];
+        
+        if(coins[n-1] > tar)
+            return dp[n][tar] = recur(coins,tar,n-1,dp);
+        
+        return dp[n][tar] = min(recur(coins,tar,n-1,dp),1 + recur(coins,tar-coins[n-1],n,dp));
+    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        int *dp = new int[amount + 1];
-        for(int i=0;i<=amount;i++) dp[i] = amount+1;
-        dp[0] = 0;
-        
-        for(int i=1;i<=amount;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(coins[j] <= i)
-                {
-                    dp[i] = min(dp[i],dp[ i - coins[j] ] + 1);
-                }
-            }
-        }
-        
-        int ans = dp[amount] > amount ? -1 : dp[amount];
-        
-        delete[] dp;
-        return ans;
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        int val = recur(coins,amount,coins.size(),dp);
+
+        return val == INT_MAX-3 ? -1 : val;
     }
 };
