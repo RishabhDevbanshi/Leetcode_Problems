5fa2b2cbaaf040a4f00ad3cd92135e8e401d7192
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2021-07-21 13:54:45 +0530

    Time: 4 ms (14.39%), Space: 9.8 MB (17.18%) - LeetHub

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,19 +1,29 @@
 class Solution {
+    bool isValid(int n,int m,int r,int c)
+    {
+        if(r<0 or c<0 or r>=n or c>=m) return false;
+        return true;
+    }
+    
+    int dfs(int n,int m,int r,int c,vector<vector<int>> &dp)
+    {
+        if(!isValid(n,m,r,c)) return dp[r][c] = 0;
+        
+        if(r == n-1 and c == m-1) 
+            return dp[r][c] = 1;
+        
+        if(dp[r][c] != -1)
+            return dp[r][c];
+        
+        int right = dfs(n,m,r,c+1,dp);
+        int down = dfs(n,m,r+1,c,dp);
+        
+        return dp[r][c] = right + down;
+    }
+    
 public:
     int uniquePaths(int n, int m) {
-        vector<vector<int>> dp(n,vector<int>(m,0));
-        
-        for(int j=0;j<m;j++) dp[n-1][j] = 1;
-        for(int i=0;i<n;i++) dp[i][m-1] = 1;
-        
-        for(int i=n-2;i>=0;i--)
-        {
-            for(int j=m-2;j>=0;j--)
-            {
-                dp[i][j] = dp[i+1][j] + dp[i][j+1];
-            }
-        }
-        
-        return dp[0][0];
+        vector<vector<int>> dp(105,vector<int>(105,-1));
+        return dfs(n,m,0,0,dp);
     }
 };
