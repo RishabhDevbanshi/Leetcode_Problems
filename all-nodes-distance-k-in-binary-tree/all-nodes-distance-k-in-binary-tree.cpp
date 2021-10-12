f815bdb0ed2d26e77cc05f04d130c3ab4c798642
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2021-10-12 20:34:46 +0530

    Time: 14 ms (13.67%), Space: 12.7 MB (63.31%) - LeetHub

--- all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -8,53 +8,77 @@
  * };
  */
 class Solution {
-    vector<int> arr;
-    
-    void trav(TreeNode *root,int k)
-    {
-        if(root == NULL or k<0) return;
-        if(k == 0) {arr.push_back(root->val); return;}
-        
-        trav(root->left,k-1);
-        trav(root->right,k-1);
-    }
-    
-    int utilAncestor(TreeNode* root,TreeNode *target,int k)
-    {
-        if(root == NULL) return -1;
-        
-        if(root == target) 
-        {
-            trav(root,k);
-            return 0;
-        }
-        
-        int dl = utilAncestor(root->left,target,k);
-        if(dl != -1)
-        {
-            if(1+dl == k) arr.push_back(root->val);
-            else trav(root->right,k-dl-2);
-            
-            return 1+dl;
-        }
-        
-        int dr = utilAncestor(root->right,target,k);
-        if(dr != -1)
-        {
-            if(1+dr == k) arr.push_back(root->val);
-            else trav(root->left,k-dr-2);
-            
-            return 1+dr;
-        }
-        
-        
-        return -1;
-    }
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        arr.clear();
-        utilAncestor(root,target,k);
-        
-        return arr;
+        if(k == 0)
+        {
+            vector<int> arr;
+            arr.push_back(target->val);
+            return arr;
+        }
+        unordered_map<TreeNode* , TreeNode*> par;
+        queue<TreeNode*> q;
+        q.push(root);
+        par[root] = NULL;
+        while(!q.empty())
+        {
+            int sz = q.size();
+            for(int i=0;i<sz;i++)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left)
+                {
+                    par[node->left] = node;
+                    q.push(node->left);
+                }
+                if(node->right)
+                {
+                    par[node->right] = node;
+                    q.push(node->right);
+                }
+            }
+        }
+        unordered_map<TreeNode*,bool> vis;
+        vector<int> res;
+        int lvl = 0;
+        q.push(target);
+        vis[target] = true;
+        while(!q.empty())
+        {
+            vector<int> arr;
+            lvl++;
+            int sz = q.size();
+            for(int i=0;i<sz;i++)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                if(node->left and !vis[node->left])
+                {
+                    arr.push_back(node->left->val);
+                    q.push(node->left);
+                    vis[node->left] = true;
+                }
+                if(node->right and !vis[node->right])
+                {
+                    arr.push_back(node->right->val);
+                    q.push(node->right);
+                    vis[node->right] = true;
+                }
+                if(par[node] and !vis[par[node]])
+                {
+                    arr.push_back(par[node]->val);
+                    q.push(par[node]);
+                    vis[par[node]] = true;
+                }
+            }
+            if(lvl == k) return arr;
+        }
+        return {};
     }
 };
+
+
+
+
+
