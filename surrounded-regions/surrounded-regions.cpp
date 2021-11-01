0c717509d8fa1cdce23100705a19284ddb6881e2
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2021-11-01 19:43:06 +0530

    Time: 8 ms (98.69%), Space: 9.9 MB (79.51%) - LeetHub

--- surrounded-regions/surrounded-regions.cpp
+++ surrounded-regions/surrounded-regions.cpp
@@ -1,54 +1,37 @@
-// for 4 directions
-int drow[4] = {1,0,-1,0};
-int dcol[4] = {0,1,0,-1};
-
 class Solution {
-    //to check if a given idx is valid and value at that idx is 'O' or not
-    bool isValid(vector<vector<char>> &board,int i,int j,int r,int c)
-    {
-        if(i>=0 and j>=0 and i<r and j<c and board[i][j] == 'O') return true;
-        
-        return false;
-    }
-    
-    //simple dfs traversal 
-    void dfs(vector<vector<char>> &board,int i,int j,int r,int c)
-    {
-        if(!isValid(board,i,j,r,c)) return;
-        board[i][j] = '1';
-        
-        for(int x=0;x<4;x++)
-        {
-            dfs(board,i+drow[x],j+dcol[x],r,c);
-        }
-    }
 public:
+    void dfs(vector<vector<char>> &board,int row,int col)
+    {
+        if(row<0 || row>=board.size() || col<0 || col>=board[0].size() || board[row][col] != 'O') return;
+        board[row][col] = 'T';
+        dfs(board,row+1,col);
+        dfs(board,row-1,col);
+        dfs(board,row,col+1);
+        dfs(board,row,col-1);
+    }
     void solve(vector<vector<char>>& board) {
-        int r = board.size() , c = board[0].size();
-        
-        //check along row boundaries
-        for(int i=0;i<r;i++)
+        int n = board.size() , m = board[0].size();
+        for(int i=0;i<board.size();i++)
         {
-            dfs(board,i,0,r,c);
-            dfs(board,i,c-1,r,c);
+            if(board[i][0] == 'O') dfs(board,i,0);
+            if(board[i][m-1] == 'O') dfs(board,i,m-1);
         }
         
-        //check along column boundaries
-        for(int i=0;i<c;i++)
+        for(int i=0;i<board[0].size();i++)
         {
-            dfs(board,0,i,r,c);
-            dfs(board,r-1,i,r,c);
+            if(board[0][i] == 'O') dfs(board,0,i);
+            if(board[n-1][i] == 'O') dfs(board,n-1,i);
         }
         
-        //change back '1' to 'O' and 'O' to 'X'
-        for(int i=0;i<r;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j=0;j<m;j++)
             {
-                if(board[i][j] == 'O') board[i][j] = 'X';
-                else if(board[i][j] == '1') board[i][j] = 'O';
+                if(board[i][j] == 'T') board[i][j] = 'O';
+                else if(board[i][j] == 'O') board[i][j] = 'X';
             }
         }
+        
         
     }
 };
