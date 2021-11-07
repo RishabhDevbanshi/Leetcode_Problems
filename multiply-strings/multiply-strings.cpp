c5a204e4fa766bbede824c0e8c21cd0477a5df94
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2021-11-07 15:15:59 +0530

    Time: 8 ms (60.22%), Space: 6.7 MB (70.82%) - LeetHub

--- multiply-strings/multiply-strings.cpp
+++ multiply-strings/multiply-strings.cpp
@@ -1,86 +1,29 @@
 class Solution {
-    string add(string &a,string &b)
-    {
-       int carry = 0;
-        // cout<<a<<" "<<b<<"\n";
-        int i=a.size()-1,j=b.size()-1;
-        string ans = "";
-        while(i>=0 && j>=0)
-        {
-            int res = (a[i] - '0') + (b[j]-'0') + carry;
-            carry = res/10;
-            int rem = res%10;
-            ans += ('0' + rem);
-            i-- , j--;
-        }
-        
-        while(i>=0)
-        {
-            int res = (a[i] - '0') + carry;
-            carry = res/10;
-            int rem = res%10;
-            ans += ('0' + rem);
-            i--;
-        }
-        
-        while(j>=0)
-        {
-            int res = (b[j]-'0') + carry;
-            carry = res/10;
-            int rem = res%10;
-            ans += ('0' + rem);
-            j--;
-        }
-        
-        if(carry) ans += ('0' + carry);
-        
-        reverse(ans.begin(),ans.end());
-        // cout<<ans<<"\n";
-        return ans;
-    }
-    
-    string mul(char &a,string &b,int idx)
-    {
-        string ans = "";
-        int val = (a - '0');
-        int carry = 0;
-        
-        for(int i=b.size()-1;i>=0;i--)
-        {
-            char ch = b[i];
-            int oth = (ch - '0');
-            int res = (val*oth) + carry;
-            carry = res/10;
-            int rem = res%10;
-            ans += ('0' + rem);
-        }
-        if(carry) ans += (carry + '0');
-        reverse(ans.begin(),ans.end());
-        for(int i=0;i<idx;i++) ans += '0';
-        return ans;
-    }
 public:
     string multiply(string num1, string num2) {
+        string st{};
+        int n = num1.size(), m = num2.size();
+        vector<int>val(n+m, 0); // result can have at max n+m values
         
-        int idx = num1.size();
-        --idx;
-        
-        string ans = "";
-        
-        for(auto &ch : num1)
-        {
-            string res = mul(ch,num2,idx--);
-            ans = add(ans,res);
+		//multiply every digit of num1 to that of num2
+        for(int i{n-1}; i>=0; --i){
+            
+            for(int j{m-1}; j>=0; --j){
+                
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                int sum = mul + val[i+j+1]; //cur multiplication + prev intermediate result in that index
+                val[i+j+1] = sum % 10; // taking the unit place part of our sum
+                val[i+j] += sum / 10; // taking the carry of our sum
+            }
         }
         
-        string res = "";
-        int i=0;
-        while(i<ans.size() && ans[i]=='0') i++;
+		//appending every number except the leading zeros
+        for(auto &num: val)
+            if(st.size() || num )
+                st.append(to_string(num));
         
-        if(i == ans.size()) return "0";
-        
-        while(i<ans.size()) res += ans[i] , i++;
-        
-        return res;
+		//If there were only zeros in val, then result is just 0
+        if(st.size() == 0) st = "0";
+        return st;
     }
 };
