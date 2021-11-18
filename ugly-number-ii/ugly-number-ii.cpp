5436dc4625e1aa62b2e72fa6bb35197ca73ea1d4
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2021-11-18 18:32:11 +0530

    Time: 108 ms (20.77%), Space: 29.5 MB (15.41%) - LeetHub

--- ugly-number-ii/ugly-number-ii.cpp
+++ ugly-number-ii/ugly-number-ii.cpp
@@ -1,31 +1,33 @@
-#define ll long long
+#define ull unsigned long long
 
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        priority_queue<ll,vector<ll>,greater<ll>> pq;
-        unordered_map<ll,int> mp;
-        pq.push(1);
-        mp[1] = 1;
+        set<ull> st;
+        
+        st.insert(1);
         
         int cnt = 0;
-        int res = -1;
         
-        while(!pq.empty())
+        while(!st.empty())
         {
-            long long val = pq.top(); pq.pop();
-            // cout<<val<<" ";
+            ull val = *st.begin();
+            st.erase(st.begin());
             cnt++;
+            // cout<<val<<" ";
+            
             if(cnt == n) return val;
             
-            if(mp.find(val * 2) == mp.end())
-                mp[val*2] = 1 , pq.push(val*2);
-            if(mp.find(val * 3) == mp.end())
-                mp[val*3] = 1 , pq.push(val*3);
-            if(mp.find(val * 5) == mp.end())
-                mp[val*5] = 1 , pq.push(val*5);
+            // if(val > 10006) continue;
+            
+            st.insert(val * 2);
+            st.insert(val * 3);
+            st.insert(val * 5);
+            
+            // for(auto &ele : st) cout<<ele<<" ";
+            // cout<<"\n";
+            
         }
-        
         
         return -1;
     }
