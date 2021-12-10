ed80c8f9deda275b8d1759f4ee81582d14b35989
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2021-12-10 11:58:10 +0530

    Time: 8 ms (64.77%), Space: 9.6 MB (67.17%) - LeetHub

--- delete-and-earn/delete-and-earn.cpp
+++ delete-and-earn/delete-and-earn.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int deleteAndEarn(vector<int>& arr) {
+    int deleteAndEarn(vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        
+        unordered_map<int,int> mp;
         int mx = 0;
-        int n = size(arr);
-        unordered_map<int, int> mp;
-        for (int i = 0; i < n; i++)
-        {
-            // cin >> arr[i];
-            mx = max(mx, arr[i]);
-            mp[arr[i]]++;
-        }
-
-        vector<int> dp(mx + 1, 0);
-
+        
+        for(auto &val : nums)
+            mp[val]++ , mx = max(mx,val);
+        
+        int n = size(nums);
+        int dp[mx+1];
+        
+        dp[0] = 0;
         dp[1] = mp.find(1) == mp.end() ? 0 : mp[1];
-
-        for (int i = 2; i < size(dp); i++)
+        
+        for(int i=2;i<=mx;i++)
         {
-            int val = mp.find(i) == mp.end() ? 0 : mp[i];
-            dp[i] = max(dp[i - 1], dp[i - 2] + (val * i));
+            int val = mp.find(i) == mp.end() ? 0 : i * mp[i];
+            dp[i] = max(dp[i-1] , val + dp[i-2]);
         }
-
-        // cout << dp.back() << "\n";
-        return dp.back();
+        
+        return  dp[mx];
     }
 };
