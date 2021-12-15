443b294cf95be35c2a3cedbdf774e5f0345a9082
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2021-12-15 13:52:16 +0530

    Time: 6 ms (14.31%), Space: 8.5 MB (72.79%) - LeetHub

---
+++ running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for(int i=1;i<size(nums);i++)
+        {
+            nums[i] += nums[i-1];
+        }
+        
+        return nums;
+    }
+};
