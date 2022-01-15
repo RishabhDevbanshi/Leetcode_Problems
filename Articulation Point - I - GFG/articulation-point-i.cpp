ef758391ce8233e52b1c817aef2745f396c2d1aa
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-01-15 13:06:17 +0530

    Added solution - LeetHub

---
+++ Articulation Point - I - GFG/articulation-point-i.cpp
@@ -0,0 +1,79 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    
+    void dfs(vector<int> g[],int par,int node,int disc[],int low[],int timer,bool mark[])
+    {
+        disc[node] = low[node] = timer++;
+        int children = 0;
+        for(auto &child : g[node])
+        {
+            // children++;
+            if(disc[child] == -1)
+            {
+                children++;
+                dfs(g,node,child,disc,low,timer,mark);
+                low[node] = min(low[node],low[child]);
+                if(par != -1 and low[child] >= disc[node])
+                    mark[node] = true;
+            }
+            else if(child != par) low[node] = min(low[node],disc[child]);
+        }
+        
+        if(par==-1 and children>1) mark[node] = true;
+        
+    }
+    
+  public:
+    vector<int> articulationPoints(int V, vector<int>adj[]) {
+        int timer = 0;
+        int disc[V] , low[V];
+        memset(disc,-1,sizeof disc);
+        memset(low,-1,sizeof low);
+        
+        bool mark[V];
+        memset(mark,false,sizeof mark);
+        
+        dfs(adj,-1,0,disc,low,timer,mark);
+        
+        vector<int> res;
+        for(int i=0;i<V;i++) 
+        {
+            if(mark[i]) res.push_back(i);
+        }
+        
+        if(res.size() < 1) res.push_back(-1);
+        
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		vector<int> ans = obj.articulationPoints(V, adj);
+		for(auto i: ans)cout << i << " ";
+		cout << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
