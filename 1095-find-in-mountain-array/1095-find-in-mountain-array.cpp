b20073225a137177ddef576b613412e07116258c
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-01-29 17:00:58 +0530

    Time: 4 ms (81.77%), Space: 7 MB (55.36%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -10,46 +10,75 @@
 
 class Solution {
 public:
-    int findInMountainArray(int target, MountainArray &A) {
-        int left = 0, right = A.length() - 1;
-        // Find the peak index
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (A.get(mid) < A.get(mid + 1)) {
-                left = mid + 1;
-            } else { 
-                right = mid;
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int n = mountainArr.length();
+        int start = 0 , end = n-1;
+        int peak , peakVal;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            int val = mountainArr.get(mid);
+            int prev= mid-1>=0 ? mountainArr.get(mid-1) : INT_MAX;
+            int next = mid+1<n ? mountainArr.get(mid+1) : INT_MAX;
+            
+            if(val > prev and val > next)
+            {
+                peak = mid;
+                peakVal = val;
+                break;
             }
+            else if(mid == 0)
+                start += 1;
+            else if(mid == n-1)
+                end -= 1;
+            else if(val < prev)
+                end = mid-1;
+            else start = mid+1;
         }
-        int peak = left;
         
-        // Binary search on increasing subarray
-        left = 0;
-        right = peak;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (A.get(mid) < target) {
-                left = mid + 1;
-            } else if (A.get(mid) > target) { 
-                right = mid - 1;
-            } else {
-                return mid;
+        // cout<<peak<<" "<<peakVal<<"\n";
+        
+        int startVal = mountainArr.get(0) , endVal = mountainArr.get(n-1);
+        
+        if(target > peakVal)
+            return -1;
+        
+        if(target == peakVal)
+            return peak;
+        
+        // cout<<peak<<" "<<peakVal<<"\n";
+        
+        if(target >= startVal and target <= peakVal)
+        {
+            int start = 0 , end = peak-1;
+            while(start <= end)
+            {
+                int mid = start + (end-start)/2;
+                int val = mountainArr.get(mid);
+                // cout<<start<<" "<<end<<" "<<val<<"\n";
+                
+                if(val == target) return mid;
+                else if(val < target) start = mid+1;
+                else end = mid-1;
             }
         }
         
-        // Binary search on decreasing subarray
-        left = peak;
-        right = A.length() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (A.get(mid) < target) {
-                right = mid - 1;
-            } else if (A.get(mid) > target) { 
-                left = mid + 1;
-            } else {
-                return mid;
+        // cout<<endVal<<"\n";
+        
+        if(target >= endVal and target <= peakVal)
+        {
+            // cout<<"here\n";
+            int start = peak+1 , end = n-1;
+            while(start <= end)
+            {
+                int mid = start + (end-start)/2;
+                int val = mountainArr.get(mid);
+                if(val == target) return mid;
+                else if(val < target) end = mid-1;
+                else start = mid+1;
             }
         }
+        
         return -1;
     }
 };
