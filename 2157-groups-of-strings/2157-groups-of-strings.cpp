02e426cf65f9580a376d29a0de52382a9d29e860
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-02-01 12:26:12 +0530

    Time: 1792 ms (26.25%), Space: 155.8 MB (20.04%) - LeetHub

---
+++ 2157-groups-of-strings/2157-groups-of-strings.cpp
@@ -0,0 +1,78 @@
+class DSU
+{
+    vector<int> par;
+    public:
+    DSU(int N)
+    {
+        par.resize(N);
+        for(int i=0;i<N;i++) par[i] = i;
+    }
+    
+    int findPar(int u)
+    {
+        if(u == par[u]) return u;
+        
+        return par[u] = findPar(par[u]);
+    }
+    
+    void unon(int u,int v)
+    {
+        u = findPar(u) , v = findPar(v);
+        
+        par[u] = v;
+    }
+};
+
+class Solution {
+public:
+    vector<int> groupStrings(vector<string>& words) {
+        int n = size(words);
+        DSU uf(n+1);
+        
+        unordered_map<int,int> mp,delMap;
+        
+        int i=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            int hash = 0;
+            for(auto &ch : words[i]) hash |= 1<<(ch-'a');
+            
+            for(int j=0;j<26;j++)
+            {
+                if(hash >> j & 1)
+                {
+                    int t_hash = hash ^ (1<<j);
+                    if(mp.count(t_hash)) uf.unon(mp[t_hash],i);
+                    if(delMap.count(t_hash)) uf.unon(delMap[t_hash],i);
+                    else delMap[t_hash] = i;
+                }
+                else
+                {
+                    int t_hash = hash | (1<<j);
+                    if(mp.count(t_hash)) uf.unon(mp[t_hash],i);
+                }
+            }
+            
+            mp[hash] = i;
+        }
+        
+        unordered_map<int,int> res;
+        for(int i=0;i<n;i++)
+        {
+            int p = uf.findPar(i);
+            // cout<<p<<" ";
+            res[p]++;
+        }
+        
+        int mx = 0;
+        for(auto &it : res)
+        {
+            mx = max(mx,it.second);
+        }
+        
+        int sz = size(res);
+        
+        return {sz,mx};
+    }
+};
