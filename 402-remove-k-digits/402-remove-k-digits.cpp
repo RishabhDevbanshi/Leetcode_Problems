4cc7b1b3e0088a6ee22ea00322a9a2e010bb0388
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-02-18 12:16:11 +0530

    Time: 4 ms (83.64%), Space: 7 MB (74.87%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,56 +1,21 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        if(k == size(num))
-            return "0";
-        
-        stack<int> st;
-        vector<int> ng(size(num),-1);
-        
-        for(int i=size(num)-1;i>=0;i--)
+        string ans = "";
+        for(auto &ch : num)
         {
-            int val = num[i] - '0';
-            while(!st.empty() and val <= (num[st.top()] - '0'))
-                st.pop();
-            if(!st.empty()) ng[i] = st.top();
-            st.push(i);
-        }
-        
-        int cnt = size(num) - k;
-        
-        string res = "";
-        
-        int tmp = 0;
-        
-        int idx = 0;
-        
-        while(size(res) < cnt and idx < size(num))
-        {
-            // ++tmp;
-            // if(tmp == 3) break;
-            int i = ng[idx];
-            if(i == -1)
-                res += num[idx++];
-            else
-            {
-                // cout<<i<<" "<<size(num)<<" "<<size(res)<<"\n";
-                if(size(num) - i >= cnt - size(res))
-                    idx = i;
-                else
-                    res += num[idx++];
-            }
+            while(size(ans) and ans.back() > ch and k)
+                ans.pop_back() , k--;
             
-            // cout<<idx<<" ";
-        }
+            if(size(ans) || ch != '0')
+                ans.push_back(ch);
+        } 
         
-        idx=0;
-        while(idx < size(res) and res[idx] == '0')
-            idx++;
         
-        if(idx == size(res))
-            return "0";
-         
-        return res.substr(idx);
+        while(size(ans) and k--)
+            ans.pop_back();
         
+        
+        return ans == "" ? "0" : ans;
     }
 };
