c530640d63def691d2187d63b1d80d42ebed7820
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-02-24 08:59:19 +0530

    Time: 144 ms (48.83%), Space: 48.6 MB (24.25%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -10,70 +10,49 @@
  */
 class Solution {
     
-    ListNode *merge(ListNode *a,ListNode *b)
-    {
-        if(!a) return b;
-        if(!b) return a;
-        
-        ListNode *head = nullptr;
-        if(a->val < b->val)
-        {
-            head = a;
-            a = a->next;
-        }
-        else
-        {
-            head = b;
-            b = b->next;
-        }
-        
-        ListNode *tail = head;
-        
-        while(a and b)
-        {
-            if(a->val < b->val)
-            {
-                tail->next = a;
-                a = a->next;
-            }
-            else
-            {
-                tail->next = b;
-                b = b->next;
-            }
-            
-            tail = tail->next;
-        }
-        
-        if(a) 
-            tail->next = a;
-        else
-            tail->next = b;
-        
-        return head;
-    }
-    
     ListNode *findMid(ListNode *head)
     {
-        ListNode *slow = head, *fast = head->next;
+        ListNode *fast = head , *slow = nullptr;
         while(fast and fast->next)
         {
-            slow = slow->next;
+            slow = slow ? slow->next : head;
             fast = fast->next->next;
         }
         
         return slow;
     }
     
+    ListNode *merge(ListNode *a,ListNode *b)
+    {
+        ListNode *tmp = new ListNode();
+        ListNode *tail = tmp;
+        
+        while(a and b)
+        {
+            if(a->val < b->val)
+                tail->next = a , a = a->next;
+            else tail->next = b , b = b->next;
+            
+            tail = tail->next;
+        }
+        
+        tail->next = a ? a : b;
+        
+        return tmp->next;
+    }
+    
 public:
     ListNode* sortList(ListNode* head) {
-        if(!head || !head->next)
+        if(!head or !head->next)
             return head;
+        ListNode *slow = findMid(head);
         
-        ListNode *mid = findMid(head);
-        ListNode *right = mid->next;
-        mid->next = NULL;
-        // cout<<right->val<<"\n";
-        return merge(sortList(head) , sortList(right));
+        ListNode *mid = slow->next;
+        slow->next = nullptr;
+        
+        ListNode *a = sortList(head);
+        ListNode *b = sortList(mid);
+        
+        return merge(a,b);
     }
 };
