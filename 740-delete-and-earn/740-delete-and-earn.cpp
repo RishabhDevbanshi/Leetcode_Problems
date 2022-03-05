2ff386e64d94ad9b4fdeb649dccfe2399cff3ae5
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-03-05 15:16:21 +0530

    Time: 33 ms (9.93%), Space: 9.7 MB (67.05%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        // sort(nums.begin(),nums.end());
         
         unordered_map<int,int> mp;
         int mx = 0;
-        
         for(auto &val : nums)
             mp[val]++ , mx = max(mx,val);
         
-        int n = size(nums);
         int dp[mx+1];
+        memset(dp,0,sizeof dp);
         
-        dp[0] = 0;
-        dp[1] = mp.find(1) == mp.end() ? 0 : mp[1];
+        dp[1] = mp.count(1) ? 1*mp[1] : 0;
         
         for(int i=2;i<=mx;i++)
         {
-            int val = mp.find(i) == mp.end() ? 0 : i * mp[i];
-            dp[i] = max(dp[i-1] , val + dp[i-2]);
+            int val = mp.count(i) ? i*mp[i] : 0;
+            dp[i] = max(dp[i-1],val + dp[i-2]);
         }
         
-        return  dp[mx];
+        for(int i=0;i<=mx;i++)
+            cout<<dp[i]<<" ";
+        
+        return dp[mx];
     }
 };
