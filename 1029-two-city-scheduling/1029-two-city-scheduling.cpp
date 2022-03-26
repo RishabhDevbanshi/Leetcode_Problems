177188103a740c1e7d8d0196785c7ca6b14a8982
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-03-26 06:20:40 +0530

    Time: 3 ms (91.21%), Space: 7.9 MB (47.19%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,36 +1,24 @@
 class Solution {
-   
-    int dp[100][100];
-   
-    int recur(vector<vector<int>> &costs,int idx,int f,int s)
-    {
-        if(idx == size(costs))
-            return 0;
-       
-        if(dp[f][s] != -1)
-            return dp[f][s];
-       
-        if(f == size(costs)>>1)
-            return dp[f][s] = costs[idx][1] + recur(costs,idx+1,f,s+1);
-        else if(s == size(costs)>>1)
-        {
-            return dp[f][s] = costs[idx][0] + recur(costs,idx+1,f+1,s);
-        }
-        else
-        {
-            int a = costs[idx][0] + recur(costs,idx+1,f+1,s);
-            int b = costs[idx][1] + recur(costs,idx+1,f,s+1);
-           
-            return dp[f][s] = min(a,b);
-        }
-       
-       
-    }
-   
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        memset(dp,-1,sizeof dp);
-        return recur(costs,0,0,0);
-       
+        
+        sort(costs.begin(),costs.end(),[&](auto &a , auto &b){
+            return a[0] - a[1] < b[0] - b[1];
+        });
+        
+        // for(auto &val : costs)
+        // {
+        //     cout << val[0] << " " << val[1] << "\n";
+        // }
+        
+        int tot = 0; 
+        
+        for(int i=0;i<size(costs)>>1;i++)
+            tot += costs[i][0];
+        
+        for(int i=size(costs)>>1;i<size(costs);i++)
+            tot += costs[i][1];
+        
+        return tot;
     }
 };
