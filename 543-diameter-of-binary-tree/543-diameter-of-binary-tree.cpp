7c7b1c6d7769eedc231579803a04be9d3082d852
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-05-05 20:57:33 +0530

    Time: 10 ms (76.90%), Space: 20.6 MB (12.72%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -1,39 +1,32 @@
-#define height first
-#define diameter second
-#define getpair pair<int,int>
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-	getpair diameterOfBinaryTreehelper(TreeNode *root) {
-		if (!root) {
-			getpair ans;
-			ans.height = 0;
-			ans.diameter = 0;
-			return ans;
-		}
-
-		getpair leftAns = diameterOfBinaryTreehelper(root->left);
-		getpair RightAns = diameterOfBinaryTreehelper(root->right);
-
-		int leftDiameter = leftAns.diameter;
-		int leftHeight = leftAns.height;
-		int RightDiameter = RightAns.diameter;
-		int RightHeight = RightAns.height;
-
-		int finalHeight = 1 + max(leftHeight, RightHeight);
-		int finalDiameter = max(leftHeight + RightHeight, max(leftDiameter, RightDiameter));
-
-		getpair finalAns;
-		finalAns.height = finalHeight;
-		finalAns.diameter = finalDiameter;
-
-		return finalAns;
-	}
-
 public:
-	int diameterOfBinaryTree(TreeNode* root) {
-		if (!root)
-			return 0;
-		return diameterOfBinaryTreehelper(root).diameter;
-	}
+    int diameterOfBinaryTree(TreeNode* root) {
+        
+        int res = 0;
+        
+        function<int(TreeNode *)> fun = [&](TreeNode *node){
+            if(!node)
+                return 0;
+            
+            int l = fun(node->left) , r = fun(node->right);
+            res = max(res,l+r);
+            
+            return 1 + max(l,r);
+        };
+        
+        fun(root);
+        
+        return res;
+    }
 };
