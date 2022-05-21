3690d71d0e76ae46ec101fca1f450c04fa232df1
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-05-22 00:31:11 +0530

    Time: 239 ms (12.06%), Space: 18.5 MB (26.03%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,40 +1,30 @@
 class Solution {
-    int recur(vector<int> &coins,int tar,int n,vector<vector<int>> &dp)
-    {
-        if(tar == 0) return dp[n][tar] = 0;
-        if(n == 0) return dp[n][tar] = INT_MAX - 3;
-        
-        if(dp[n][tar] != -1)
-                return dp[n][tar];
-        
-        if(coins[n-1] > tar)
-            return dp[n][tar] = recur(coins,tar,n-1,dp);
-        
-        return dp[n][tar] = min(recur(coins,tar,n-1,dp),1 + recur(coins,tar-coins[n-1],n,dp));
-    }
 public:
-    int coinChange(vector<int>& arr, int amt) {
-        int n = arr.size();
-        // vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+    int coinChange(vector<int>& coins, int amount) {
+        int dp[15][100000];
+        memset(dp,-1,sizeof dp);
         
-        int dp[n+1][amt+1];
-
-        for(int i=1;i<=amt;i++) dp[0][i] = (int)1e5;
-        for(int i=0;i<=n;i++) dp[i][0] = 0;
-
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=amt;j++)
-            {
-                if(arr[i-1] > j)
-                    dp[i][j] = dp[i-1][j];
-                else
-                    dp[i][j] = min(dp[i-1][j],1 + dp[i][j - arr[i-1]]);
-            }
-        }
+        function<int(int,int)> fun = [&](int idx, int amt){
+            if(amt == 0)
+                return 0;
+            if(idx == size(coins))
+                return 1000000;
+            
+            if(dp[idx][amt] != -1)
+                return dp[idx][amt];
+            
+            if(coins[idx] > amt)
+                return dp[idx][amt] = fun(idx+1,amt);
+            
+            int op1 = fun(idx+1,amt);
+            int op2 = 1 + fun(idx,amt - coins[idx]);
+            
+            return dp[idx][amt] = min(op1,op2);
+        };
         
-        int val = dp[n][amt];
         
-        return val == (int)1e5 ? -1 : val;
+        int val = fun(0,amount);
+        
+        return val == 1000000 ? -1 : val;;
     }
 };
