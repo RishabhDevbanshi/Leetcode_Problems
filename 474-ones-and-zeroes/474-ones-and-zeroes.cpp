e898ca7424042b0d597e2d12fc05d1e85490b560
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-05-23 14:00:42 +0530

    Time: 231 ms (87.11%), Space: 32.6 MB (57.21%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,35 +1,29 @@
 class Solution {
-    int dp[105][105][605];
-    
-    int recur(vector<string> &strs,int m,int n,int i)
-    {
-        if(i == strs.size() or (m==0 and n==0)) return 0;
-        
-        if(dp[m][n][i] != -1) 
-            return dp[m][n][i];
-        
-        int zero = 0 , one = 0;
-        for(auto ele : strs[i]) 
-        {
-            zero += ele == '0';
-            one += ele == '1';
-        }
-        
-        if(zero > m or one > n)
-            return dp[m][n][i] = recur(strs,m,n,i+1);
-        
-        return dp[m][n][i] = max(recur(strs,m,n,i+1),1 + recur(strs,m-zero,n-one,i+1));
-    }
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
-        for(int i=0;i<=m;i++)
-        {
-            for(int j=0;j<=n;j++)
-            {
-                for(int k=0;k<strs.size();k++) dp[i][j][k] = -1;
-            }
-        }
+    int findMaxForm(vector<string>& str, int m, int n) {
         
-        return recur(strs,m,n,0);
+        
+        int dp[m+1][n+1][size(str)+1];
+        memset(dp,-1,sizeof dp);
+        
+        function<int(int,int,int)> fun = [&](int m,int n,int idx){
+            if(idx == size(str) || (m == 0 and n == 0))
+                return 0;
+            
+            if(dp[m][n][idx] != -1)
+                return dp[m][n][idx];
+            
+            int co = 0 , cz = 0;
+            for(auto &ch : str[idx])
+                ch == '0' ? cz++ : co++;
+            
+            if(cz > m || co > n)
+                return dp[m][n][idx] = fun(m,n,idx+1);
+            else
+                return dp[m][n][idx] = max(fun(m,n,idx+1),1 + fun(m-cz,n-co,idx+1));
+        };
+        
+        
+        return fun(m,n,0);
     }
 };
