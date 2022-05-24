c2fe748c80c3baa4677d6db379c266a8e8d5c2ad
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-05-24 13:41:42 +0530

    Time: 42 ms (6.07%), Space: 18.4 MB (5.11%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -9,29 +9,30 @@ public:
         auto fun = [&](string s,bool ok){
             vector<vector<int>> dp(size(s)+1,vector<int>(2));
         
+            int c = 0, o = 0;
+            
             int res = 0;
 
             for(int i=1;i<=size(s);i++)
             {
-                dp[i][0] = dp[i-1][0] , dp[i][1] = dp[i-1][1];
                 if(s[i-1] == ')')
-                    dp[i][1]++;
+                    c++;
                 else
-                    dp[i][0]++;
+                    o++;
 
                 if(ok)
                 {
-                    if(dp[i][0] < dp[i][1])
-                        dp[i][0] = 0 , dp[i][1] = 0;
+                    if(o < c)
+                        o = c = 0;
                 }
                 else
                 {
-                    if(dp[i][0] > dp[i][1])
-                        dp[i][0] = 0 , dp[i][1] = 0;
+                    if(o > c)
+                        o = c = 0;
                 }
 
-                if(dp[i][0] == dp[i][1])
-                    res = max(res,2 * dp[i][0]);
+                if(o == c)
+                    res = max(res,2 * o);
             }
             
             return res;
