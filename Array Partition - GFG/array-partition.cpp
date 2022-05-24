7f3f84291e7a58de0b2b6cff9f234499c0530716
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-05-24 13:09:19 +0530

    Added solution - LeetHub

--- Array Partition - GFG/array-partition.cpp
+++ Array Partition - GFG/array-partition.cpp
@@ -10,23 +10,25 @@ using namespace std;
 class Solution{
     public:
     bool partitionArray(int N, int K, int M, vector<int> &A){
+        // code here
         sort(A.begin(),A.end());
-        A.insert(A.begin(),0);
-        vector<int> dp(A.size(),0);
-        dp[0] = 1;
-        vector<int> pre(A.size(),0);
-        pre[0] = 1;
-        for(int i=1; i<A.size(); i++)
-        {
-            int l1 = lower_bound(begin(A),end(A),A[i] - M) - A.begin();
-            int l2 = i - K + 1;
-            int cur = 0;
-            if(l2 > 0) cur += pre[l2-1];
-            if(l1 >=2) cur -= pre[l1-2];
-            if(cur > 0) dp[i] = 1;
-            pre[i] = pre[i-1] + dp[i];
-        }
-        return dp[A.size()-1];
+        
+        function<bool(int)> fun = [&](int start){
+            if(start >= A.size())
+                return true;
+            for(int i=start + K - 1;i<A.size();i++)
+            {
+                if(A[i] - A[start] > M)
+                    return false;
+                bool ok = fun(i+1);
+                if(ok)
+                    return true;
+            }
+            
+            return false;
+        };
+        
+        return fun(0);
     }
 };
 
