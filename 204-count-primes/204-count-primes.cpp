0cb37606ebb9c3fa26b7110145670ca41d5eb4d6
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-05-30 17:09:59 +0530

    Time: 400 ms (30.27%), Space: 169.5 MB (6.90%) - LeetHub

---
+++ 204-count-primes/204-count-primes.cpp
@@ -0,0 +1,17 @@
+#define ll long long
+class Solution {
+    vector<ll> sieve(int n) 
+    {
+        int*arr = new int[n + 1](); 
+        vector<ll> vect; 
+        for (int i = 2; i < n; i++)if (arr[i] == 0) {vect.push_back(i); 
+        for (int j = 2 * i; j < n; j += i)arr[j] = 1;} 
+        return vect;
+    }
+
+public:
+    int countPrimes(int n) {
+        vector<ll> ans = sieve(n);
+        return ans.size();
+    }
+};
