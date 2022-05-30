da0d097983dda520df44f51498baf94cc18b6727
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-05-30 17:13:44 +0530

    Time: 193 ms (87.24%), Space: 10.4 MB (35.51%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -4,14 +4,30 @@ class Solution {
     {
         int*arr = new int[n + 1](); 
         vector<ll> vect; 
-        for (int i = 2; i < n; i++)if (arr[i] == 0) {vect.push_back(i); 
-        for (int j = 2 * i; j < n; j += i)arr[j] = 1;} 
+        for (int i = 2; i < n; i++)
+        {
+            if (arr[i] == 0)
+            {
+                vect.push_back(i); 
+                for (int j = 2 * i; j < n; j += i)
+                arr[j] = 1;
+            } 
+        }
         return vect;
     }
 
 public:
     int countPrimes(int n) {
-        vector<ll> ans = sieve(n);
-        return ans.size();
+        vector<bool> v (n+1, true);
+        v[0] = v[1] =  false;
+        int cnt = 0;
+        for (long long int i = 2; i < n; i++){
+            if (v[i]){
+                cnt++;
+                for (long long int j = i*i; j < n; j+=i )
+                    v[j] = false;
+            }
+        }
+        return cnt;  
     }
 };
