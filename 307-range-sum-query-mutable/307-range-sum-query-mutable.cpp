d68e3458e25a7fa3d4a1eb5681ca2ee9ada8668e
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-06-08 11:26:49 +0530

    Time: 562 ms (63.23%), Space: 150.7 MB (72.44%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,68 +1,44 @@
 class NumArray {
-    vector<int> seg;
+    vector<int> bit;
     int n;
+    vector<int> arr;
 public:
     NumArray(vector<int>& nums) {
         n = size(nums);
-        seg.resize(4*n);
-         
-        function<void(int,int,int)> build = [&](int idx,int left,int right){
-            if(left == right)
-            {
-                seg[idx] = nums[left];
-                return;
-            }
-            
-            int mid = (left + right)>>1;
-            build(2*idx + 1,left,mid);
-            build(2*idx + 2,mid+1,right);
-            
-            seg[idx] = seg[2*idx + 1] + seg[2*idx + 2];
-        };
+        bit.resize(n+1);
+        arr = nums;
         
+        for(int i=0;i<size(nums);i++)
+        {
+            for(int j=i+1;j<=n;j += (j & -j))
+                bit[j] += nums[i];
+        }
         
-        build(0,0,n-1);
     }
     
     void update(int index, int val) {
         
-        function<void(int,int,int)> upd = [&](int idx,int left,int right){
-            if(left == right)
-            {
-                seg[idx] = val;
-                return;
-            }
-            
-            int mid = (left + right)>>1;
-            
-            if(left <= index and index <= mid)
-                upd(2*idx + 1,left,mid);
-            else
-                upd(2*idx + 2,mid+1,right);
-            
-            seg[idx] = seg[2*idx + 1] + seg[2*idx + 2];
-        };
+        for(int i=index+1;i<=n;i+=(i & -i))
+            bit[i] += (val - arr[index]);
         
-        upd(0,0,n-1);
+        arr[index] = val;
     }
     
     int sumRange(int left, int right) {
+        left++ , right++;
         
-        function<int(int,int,int)> query = [&](int idx,int l,int r){
-            if(left <= l and r <= right)
-                return seg[idx];
-            if(r < left or right < l)
-                return 0;
+        auto query = [&](int idx){
             
-            int mid = (l + r) >>1; 
+            int tot = 0;
             
-            int left_ans = query(2*idx + 1,l,mid);
-            int right_ans = query(2*idx + 2,mid+1,r);
+            for(int i=idx;i>0;i -= (i & -i))
+                tot += bit[i];
             
-            return left_ans + right_ans;
+            return tot;
         };
         
-        return query(0,0,n-1);
+        
+        return query(right) - query(left-1);
     }
 };
 
