4643c60053cc212abe1f1c8ffb4ad13de3e2755f
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-06-12 00:33:37 +0530

    Time: 1244 ms (40.00%), Space: 18.1 MB (40.00%) - LeetHub

---
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool matchReplacement(string s, string sub, vector<vector<char>>& mappings) {
+        map<char,unordered_set<char>> mp;
+        for(auto &v : mappings)
+            mp[v[0]].insert(v[1]);
+        
+        for(int i=0;i<size(s);i++)
+        {
+            int j=0 , k=i;
+            while(j<size(sub) and k<size(s))
+            {
+                if(sub[j] == s[k] || mp[sub[j]].find(s[k]) != mp[sub[j]].end())
+                    k++ , j++;
+                else
+                {
+                    break;
+                }
+            }
+            
+            if(j == size(sub))
+                return true;
+        }
+        
+        return false;
+    }
+};
