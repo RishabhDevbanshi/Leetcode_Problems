9022bef174b4a68053cf57ad84ecb7d71fe84ac2
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-07-12 12:52:57 +0530

    Time: 740 ms (28.57%), Space: 10.1 MB (47.49%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -5,11 +5,10 @@ public:
         if(size(matchsticks) < 4 || sum % 4 != 0)
             return false;
         
-        int vis = 0;
+        sort(matchsticks.begin(),matchsticks.end());
+        int bit_dp = 0;
         
         int req = sum/4;
-        
-        sort(matchsticks.begin(),matchsticks.end());
         
         function<bool(int,int,int)> dfs = [&](int sum,int num,int i){
             if(num == 1)
@@ -20,16 +19,16 @@ public:
             
             for(int j=i;j<size(matchsticks);j++)
             {
-                int bit = (vis>>j)&1;
+                int bit = (bit_dp>>j)&1;
                 if(bit || sum+matchsticks[j]>req)
                     continue;
-                vis = vis xor (1<<j);
+                bit_dp = bit_dp xor (1<<j);
                 bool ok = dfs(sum+matchsticks[j],num,j+1);
                 
                 if(ok)
                     return true;
                 
-                vis = vis xor (1<<j);
+                bit_dp = bit_dp xor (1<<j);
             }
             
             return false;
