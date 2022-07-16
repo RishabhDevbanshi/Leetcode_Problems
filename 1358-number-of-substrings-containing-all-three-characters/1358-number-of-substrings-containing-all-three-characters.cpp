2b2c65c4b8ad40ae5a9ab83b0797775f508e0f6f
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-07-16 14:27:39 +0530

    Time: 31 ms (67.60%), Space: 8.5 MB (87.22%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int left = 0;
-        int tot = 0;
         
-        vector<int> cnt(3);
+        auto atMost = [&](int k){
+            int left=0 , tot=0;
+            vector<int> cnt(3);
+            for(int right=0;right<size(s);right++)
+            {
+                cnt[s[right]-'a']++;
+                while((cnt[0]>0) + (cnt[1]>0) + (cnt[2]>0) > k)
+                   cnt[s[left] - 'a']-- , left++;
+                tot += (right - left + 1);
+            }
+            
+            return tot;
+        };
         
-        for(int right=0;right<size(s);right++)
-        {
-            cnt[s[right] - 'a']++;
-            while(cnt[0] and cnt[1] and cnt[2])
-                cnt[s[left]-'a']-- , left++;
-            tot += left;
-        }
+        long n = size(s);
+        long all = (n * (n+1))/2;
         
-        return tot;
+        return all - atMost(2);
     }
 };
