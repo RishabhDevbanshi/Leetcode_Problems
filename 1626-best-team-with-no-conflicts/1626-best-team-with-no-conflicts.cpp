2c73012e417ae73f8ed5de7f75d5071c1738e0bb
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-07-22 16:08:51 +0530

    Time: 1374 ms (5.00%), Space: 133.4 MB (11.68%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -9,27 +9,29 @@ public:
             arr[i] = {ages[i],scores[i]};
         }
         
-        sort(arr.begin(),arr.end(),[&](auto &a,auto &b){
-            if(a.first == b.first)
-                return a.second < b.second;
-            return a.first < b.first;
-        });
+        sort(arr.begin(),arr.end());
         
-        vector<int> dp(size(ages));
+        vector<vector<int>> dp(size(ages)+1 , vector<int>(size(ages)+2,-1));
         
-        for(int i=0;i<size(ages);i++)
-        {
-            int maxi = 0;
-            for(int j=0;j<i;j++)
-            {
-                if(arr[i].second >= arr[j].second)
-                    maxi = max(maxi,dp[j]);
-            }
+        function<int(int,int)> memo = [&](int idx,int prev){
+            if(idx == size(arr))
+                return 0;
             
-            dp[i] = maxi + arr[i].second;
-        }
+            if(dp[idx][prev] != -1)
+                return dp[idx][prev];
+            
+            if(prev != size(ages) and arr[prev].first < arr[idx].first and arr[prev].second > arr[idx].second)
+                return dp[idx][prev] = memo(idx+1,prev);
+            if(prev != size(ages) and arr[prev].first == arr[idx].first)
+            {
+                int op2 = memo(idx+1,prev);
+                int op1 = arr[idx].second + memo(idx+1,arr[idx].second > arr[prev].second ? idx : prev);
+                
+                return dp[idx][prev] = max(op1,op2);
+            }
+            return dp[idx][prev] = max(memo(idx+1,prev) ,arr[idx].second + memo(idx+1,idx));
+        };
         
-        
-        return *max_element(dp.begin(),dp.end());
+        return memo(0,size(ages));
     }
 };
