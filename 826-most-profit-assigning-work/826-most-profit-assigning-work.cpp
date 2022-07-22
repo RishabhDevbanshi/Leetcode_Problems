e4ead6d76ad8de28ea6d2637d3fec06fe307ce43
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-07-22 12:53:05 +0530

    Time: 136 ms (41.42%), Space: 36.1 MB (94.15%) - LeetHub

--- 826-most-profit-assigning-work/826-most-profit-assigning-work.cpp
+++ 826-most-profit-assigning-work/826-most-profit-assigning-work.cpp
@@ -7,39 +7,17 @@ public:
             arr[i] = {difficulty[i],profit[i]};
         }
         
-        sort(arr.begin(),arr.end());
         int tot=0;
-        int maxi=0;
         
-        for(auto &[d,p] : arr)
+        sort(arr.begin(),arr.end());
+        sort(worker.begin(),worker.end());
+        
+        int i=0 , best = 0;
+        for(auto &val : worker)
         {
-            maxi = max(maxi,p);
-            p = maxi;
-        }
-        
-        auto bs = [&](int tar){
-            int start=0, end = size(arr)-1;
-            int ans = -1;
-            while(start <= end)
-            {
-                int mid = (start + end)>>1;
-                if(arr[mid].first > tar)
-                    end = mid-1;
-                else
-                {
-                    ans = ans == -1 ? mid : max(ans,mid);
-                    start = mid+1;
-                }
-            }
-            
-            return ans;
-        };
-        
-        for(auto &ele : worker)
-        {
-            int idx = bs(ele);
-            if(idx != -1)
-                tot += arr[idx].second;
+            while(i<size(arr) and arr[i].first<=val)
+                best = max(best,arr[i++].second);
+            tot += best;
         }
         
         
