9f7a58bbacb8eeda968a8119df0f057dbcde4a18
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-08-07 17:29:36 +0530

    Time: 164 ms (93.55%), Space: 13.9 MB (85.82%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -3,6 +3,5 @@ class Solution:
         a,e,i,o,u = 1,1,1,1,1
         mod = 1e9 + 7
         for _ in range(1,n):
-            na,ne,ni,no,nu = (e+i+u)%mod , (a+i)%mod , (o+e)%mod, (i)%mod , (i+o)%mod
-            a,e,i,o,u = na,ne,ni,no,nu
+            a,e,i,o,u = (e+i+u)%mod , (a+i)%mod , (o+e)%mod, (i)%mod , (i+o)%mod
         return int((a+e+i+o+u)%mod)
