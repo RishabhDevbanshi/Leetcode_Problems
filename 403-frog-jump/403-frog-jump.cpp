fb16dbc71da60c859f6933b45acf95538ab885b2
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-09-05 16:34:20 +0530

    Time: 166 ms (75.24%), Space: 236.1 MB (8.50%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -6,14 +6,16 @@ public:
         for(int i=0;i<size(stones);i++)
             st[stones[i]] = i;
         
-        map<pair<int,int>,bool> dp;
+        // map<pair<int,int>,bool> dp;
+        int n = size(stones);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         
-        function<bool(int,int)> dfs = [&](int idx,int prev){
+        function<int(int,int)> dfs = [&](int idx,int prev)->int{
             if(idx == size(stones)-1)
               return true;
             
-            if(dp.find({idx,prev}) != dp.end())
-                return dp[{idx,prev}];
+            if(dp[idx][prev] != -1)
+                return dp[idx][prev];
             
             bool ok = false;
             
@@ -30,7 +32,7 @@ public:
                 ok |= dfs(st[stones[idx]+prev+1],prev+1);
             }
             
-            return dp[{idx,prev}] = ok;
+            return dp[idx][prev] = ok;
         };
         
         return dfs(0,0);
