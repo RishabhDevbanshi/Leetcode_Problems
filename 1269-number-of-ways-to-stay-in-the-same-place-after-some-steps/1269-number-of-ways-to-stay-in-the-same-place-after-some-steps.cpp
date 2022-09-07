fa7f1c3288fdf797b701185074e79b18d6531eea
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-09-07 19:45:46 +0530

    Time: 1462 ms (5.16%), Space: 78.6 MB (5.16%) - LeetHub

---
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numWays(int steps, int arrLen) {
+        
+        const int mod = 1e9 + 7;
+        // vector<vector<long>> dp(arrLen+1,vector<long>(steps+1,-1));
+        map<pair<long,long>,long> mp;
+        
+        function<long(int,int)> dfs = [&](int idx,int key)->long{
+            if(idx == 0 and key==0)
+                return 1;
+            if(key == 0 || idx < 0 || idx >= arrLen)
+                return 0;
+            
+            // if(dp[idx][key] != -1)
+                // return dp[idx][key];
+            
+            if(mp.find({idx,key}) != mp.end())
+                return mp[{idx,key}];
+            
+            long op3 = dfs(idx,key-1)%mod;
+            long op1 = dfs(idx+1,key-1)%mod;
+            long op2 = dfs(idx-1,key-1)%mod;
+            
+            return mp[{idx,key}] = (op1 + op2 + op3)%mod;
+        };
+        
+        return dfs(0,steps)%mod;
+    }
+};
