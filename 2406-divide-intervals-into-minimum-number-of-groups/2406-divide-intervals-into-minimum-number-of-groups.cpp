97920e68ec69aa1e25b2a1986219af0c9f281df1
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-09-14 17:56:59 +0530

    Time: 1169 ms (34.95%), Space: 108.9 MB (33.99%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());   
+        sort(intervals.begin(),intervals.end(),[&](auto &a,auto &b){
+            return a[0] == b[0] ? a[1] < b[1] : a[0] < b[0];
+        });   
         
         int cnt=0;
-        priority_queue<int,vector<int>,greater<int>> pq;
+        multiset<int> st;
         
         for(auto &v : intervals)
         {
-            while(!pq.empty() and pq.top() < v[0])
-                pq.pop();
-            pq.push(v[1]);
-            // cout << pq.top() << "\n";
-            cnt = max<int>(cnt,size(pq));
+            // cout << v[0] << " " << v[1] << "\n";
+            if(size(st))
+            {
+                auto it = st.lower_bound(v[0]);
+                // if(it == st.end())
+                // {
+                //     st.insert(v[1]);
+                //     // st.insert(v[1]);
+                //     cnt = max<int>(cnt,size(st));
+                //     continue;
+                // }
+                if(it == st.begin())
+                {
+                    st.insert(v[1]);
+                    cnt = max<int>(cnt,size(st));
+                    continue;
+                }
+                --it;
+                if(it != st.end()) st.erase(it); 
+                st.insert(v[1]); 
+                // cout << "set -> ";
+                // for(auto &val : st) cout << val << " ";
+                // cout << "\n";
+            }
+            else st.insert(v[1]);
+            cnt = max<int>(cnt,size(st));
         }
-        
+        // st.insert(v[1]);
+                    cnt = max<int>(cnt,size(st));
         
         return cnt;
         
