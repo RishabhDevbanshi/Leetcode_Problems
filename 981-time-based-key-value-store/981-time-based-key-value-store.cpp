c5ecb8a065adff781783df7ffe0fb436dadedd23
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-10-10 00:32:54 +0530

    Time: 450 ms (82.73%), Space: 128.6 MB (60.07%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,42 +1,38 @@
 class TimeMap {
+    unordered_map<string,vector<pair<int,string>>> mp;
 public:
-    unordered_map<string, vector<pair<int, string>>> keyTimeMap;
     TimeMap() {
+        
     }
     
     void set(string key, string value, int timestamp) {
-        // Push '(timestamp, value)' pair in 'key' bucket.
-        keyTimeMap[key].push_back({ timestamp, value });
+        mp[key].push_back({timestamp,value});
     }
     
     string get(string key, int timestamp) {
-        // If the 'key' does not exist in map we will return empty string.
-        if (keyTimeMap.find(key) == keyTimeMap.end()) {
-            return "";
-        }
+        int start = 0 , end = size(mp[key])-1;
         
-        if (timestamp < keyTimeMap[key][0].first) {
-            return "";
-        }
+        int idx = -1;
         
-        // Using binary search on the array of pairs.
-        int left = 0;
-        int right = keyTimeMap[key].size();
-        
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (keyTimeMap[key][mid].first <= timestamp) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        
-        // If iterator points to first element it means, no time <= timestamp exists.
-        if (right == 0) {
-            return "";
-        }
+        while(start <= end)
+        {
+            int mid = (start + end)>>1;
+            if(mp[key][mid].first == timestamp)
+                return mp[key][mid].second;
+            else if(mp[key][mid].first < timestamp)
+                idx = mid , start = mid+1;
+            else
+                end = mid - 1;
                 
-        return keyTimeMap[key][right - 1].second;
+        }
+        
+        return idx == -1 ? "" : mp[key][idx].second;
     }
 };
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
