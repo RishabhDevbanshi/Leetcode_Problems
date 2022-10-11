f693ca31eecec626c497edd104b2766f297a4d80
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-10-11 22:41:51 +0530

    Time: 98 ms (70.83%), Space: 66.5 MB (5.67%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        multiset<int> b;
-        for(auto &val : nums)
-            b.insert(val);
+        vector<int> b;
+        int maxi = nums.back();
+        for(int i=size(nums)-1;i>=0;i--)
+            b.push_back(maxi = max(maxi,nums[i]));
+        
+        reverse(b.begin(),b.end());
+        
         int mini = nums[0];
+        int idx = 0;
         for(auto &val : nums)
         {
-            b.erase(b.find(val));
-            if(mini < val and size(b) and *--b.end() > val)
+            if(mini < val and size(b) and idx < size(nums)-1 and b[idx+1] > val)
+            {
+                // cout << mini << " " << val << " " << b[idx+1] << "\n";
                 return true;
+            }
             // a.insert(val);
             mini = min(mini,val);
+            ++idx;
         }
         
         return false;
