511dba1a3d822b555f70900e9f53a82084a13d97
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-10-11 22:43:17 +0530

    Time: 162 ms (24.40%), Space: 61.6 MB (67.34%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    bool increasingTriplet(vector<int>& nums) {
-        vector<int> b;
-        int maxi = nums.back();
-        for(int i=size(nums)-1;i>=0;i--)
-            b.push_back(maxi = max(maxi,nums[i]));
-        
-        reverse(b.begin(),b.end());
-        
-        int mini = nums[0];
-        int idx = 0;
-        for(auto &val : nums)
-        {
-            if(mini < val and size(b) and idx < size(nums)-1 and b[idx+1] > val)
-            {
-                // cout << mini << " " << val << " " << b[idx+1] << "\n";
-                return true;
-            }
-            // a.insert(val);
-            mini = min(mini,val);
-            ++idx;
+   bool increasingTriplet(vector<int>& nums) {
+    int c1 = INT_MAX, c2 = INT_MAX;
+    for (int x : nums) {
+        if (x <= c1) {
+            c1 = x;           // c1 is min seen so far (it's a candidate for 1st element)
+        } else if (x <= c2) { // here when x > c1, i.e. x might be either c2 or c3
+            c2 = x;           // x is better than the current c2, store it
+        } else {              // here when we have/had c1 < c2 already and x > c2
+            return true;      // the increasing subsequence of 3 elements exists
         }
-        
-        return false;
     }
+    return false;
+}
 };
