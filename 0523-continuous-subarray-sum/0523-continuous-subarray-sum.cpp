605d1ecc022be5908d53caa80fb8e694afdc1c9a
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-10-27 00:11:17 +0530

    Time: 238 ms (93.96%), Space: 113.9 MB (26.56%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-    	// initialize the hash map with index 0 for sum 0
-        unordered_map<int, int> hashMap{{0, 0}};
-        int sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            // if the remainder sum % k occurs for the first time
-            if (!hashMap.count(sum % k))
-                hashMap[sum % k] = i + 1;
-            // if the subarray size is at least two
-            else if (hashMap[sum % k] < i)
+        unordered_map<int,int> mp;
+        mp[0] = 0;
+        int curr = 0;
+        for(int i=0;i<size(nums);i++)
+        {
+            curr += nums[i];
+            if(mp.find(curr%k) == mp.end())
+                mp[curr%k] = i+1;
+            else if(mp[curr%k] < i)
                 return true;
         }
+        
         return false;
     }
 };
