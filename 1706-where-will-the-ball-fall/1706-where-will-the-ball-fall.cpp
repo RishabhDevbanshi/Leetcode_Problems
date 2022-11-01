914cf86f918d3f9735e28cec3cbec0da20d6f788
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2022-11-01 23:43:50 +0530

    Time: 72 ms (11.03%), Space: 13 MB (99.77%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,21 +1,50 @@
 class Solution {
 public:
     vector<int> findBall(vector<vector<int>>& grid) {
-        vector<int> result(grid[0].size(), 0);
-
-        for (int col = 0; col < grid[0].size(); col++) {
-            int currentCol = col;
-            for (int row = 0; row < grid.size(); row++) {
-                int nextColumn = currentCol + grid[row][currentCol];
-                if (nextColumn < 0 || nextColumn > grid[0].size() - 1 ||
-                    grid[row][currentCol] != grid[row][nextColumn]) {
-                    result[col] = -1;
+        vector<int> res;
+        int cnt = 0;
+        for(int j=0;j<size(grid[0]);j++)
+        {
+            ++cnt;
+            // if(cnt == 3)
+            //         break;
+            int prow = -1, pcol = -1;
+            int row = 0 , col = j , last = 0;
+            // int row=0;
+            while(row<size(grid))
+            {
+                int nrow = -1 , ncol = -1;
+                if(grid[row][col] == 1)
+                {
+                    if(last == grid[row][col])
+                        nrow = row+1 , ncol = col , last = 0;
+                    else
+                        nrow = row , ncol = col+1 , last = 1;
+                }
+                else
+                {
+                    // int nrow = -1 , ncol = -1;
+                    if(last == grid[row][col])
+                        nrow = row+1 , ncol = col , last = 0;
+                    else
+                        nrow = row , ncol = col-1 , last = -1;
+                }
+                
+                if(ncol >= size(grid[0]) || (nrow == prow and ncol == pcol))
+                {
+                    col = -1;
                     break;
                 }
-                result[col] = nextColumn;
-                currentCol = nextColumn;
+                prow = row , pcol = col;
+                row = nrow , col = ncol;
+                // cout << row << " " << col << " " << last << "\n";
             }
+            // cout << "\n";
+            
+            res.push_back(col);
+            // break;
         }
-        return result;
+        
+        return res;
     }
 };
