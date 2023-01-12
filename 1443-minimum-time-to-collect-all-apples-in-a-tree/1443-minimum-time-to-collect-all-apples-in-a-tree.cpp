ab552145b9210cd4af5244f78704c868ecc43aea
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2023-01-12 23:47:36 +0530

    Time: 182 ms (92.05%), Space: 60.3 MB (97.35%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    int dfs(int node, int parent, vector<vector<int>>& adj, vector<bool>& hasApple) {
-        int totalTime = 0, childTime = 0;
-        for (auto child : adj[node]) {
-            if (child == parent) continue;
-            childTime = dfs(child, node, adj, hasApple);
-            // childTime > 0 indicates subtree of child has apples. Since the root node of the
-            // subtree does not contribute to the time, even if it has an apple, we have to check it
-            // independently.
-            if (childTime || hasApple[child]) totalTime += childTime + 2;
-        }
-
-        return totalTime;
-    }
-
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-        vector<vector<int>> adj(n);
-        for (auto& edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+        vector<int> g[n];
+        for(auto &edge : edges)
+        {
+            g[edge[0]].push_back(edge[1]);
+            g[edge[1]].push_back(edge[0]);
         }
-        return dfs(0, -1, adj, hasApple);
+        
+        function<int(int,int,int)> dfs = [&](int node,int par,int myCost){
+            
+            int childCost = 0;
+            for(auto &child : g[node])
+            {
+                if(child == par)
+                    continue;
+                childCost += dfs(child,node,2);
+            }
+            
+            if(childCost == 0 and !hasApple[node])
+                return 0;
+            
+            return childCost + myCost;
+        };
+        
+        
+        return dfs(0,-1,0);
+        
     }
 };
