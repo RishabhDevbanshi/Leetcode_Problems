4741346ca8d70d5e47c00dc60ce01b42fa096027
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2023-01-31 23:10:45 +0530

    Time: 911 ms (7.14%), Space: 133.8 MB (14.61%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        vector<pair<int,int>> arr(size(ages));
-        int maxi = 0;
-        
-        for(int i=0;i<size(ages);i++)
-        {
+        vector<pair<int,int>> arr(size(scores));
+        for(int i=0;i<size(scores);i++)
             arr[i] = {ages[i],scores[i]};
-        }
         
         sort(arr.begin(),arr.end());
         
-        vector<vector<int>> dp(size(ages)+1 , vector<int>(size(ages)+2,-1));
+        vector<vector<int>> dp(size(arr)+3,vector<int>(size(arr)+3,-1));
         
-        function<int(int,int)> memo = [&](int idx,int prev){
+        function<int(int,int)> dfs = [&](int prev,int idx){
             if(idx == size(arr))
                 return 0;
-            
-            if(dp[idx][prev] != -1)
-                return dp[idx][prev];
-            
-            if(prev != size(ages) and arr[prev].first < arr[idx].first and arr[prev].second > arr[idx].second)
-                return dp[idx][prev] = memo(idx+1,prev);
-            if(prev != size(ages) and arr[prev].first == arr[idx].first)
-            {
-                int op2 = memo(idx+1,prev);
-                int op1 = arr[idx].second + memo(idx+1,arr[idx].second > arr[prev].second ? idx : prev);
-                
-                return dp[idx][prev] = max(op1,op2);
-            }
-            return dp[idx][prev] = max(memo(idx+1,prev) ,arr[idx].second + memo(idx+1,idx));
+            if(dp[prev+1][idx+1] != -1)
+                return dp[prev+1][idx+1];
+            int op1 = dfs(prev,idx+1);
+            int op2 = 0;
+            if(prev == -1 or arr[idx].first == arr[prev].first or arr[idx].second >= arr[prev].second)
+                op2 = arr[idx].second + dfs(idx,idx+1);
+            return dp[prev+1][idx+1] = max(op1,op2);
         };
         
-        return memo(0,size(ages));
+        
+        return dfs(-1,0);
     }
 };
