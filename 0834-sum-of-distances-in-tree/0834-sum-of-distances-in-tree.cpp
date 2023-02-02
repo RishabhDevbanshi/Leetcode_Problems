02bd278b697505cc5b87d7efac8bb7c380fca5d2
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2023-02-03 01:33:09 +0530

    Time: 351 ms (38.98%), Space: 102.6 MB (33.85%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -1,38 +1,52 @@
 class Solution {
 public:
-    vector<vector<int>> v;
-    vector<int> counter, res;
-        
-    void dfs(int i, int p = -1) {
-        for(auto u : v[i]) {
-            if(u == p) continue;
-            dfs(u, i);
-            counter[i] += counter[u];
-            res[i] += res[u] + counter[u];
-        }
-        counter[i] += 1;
-    }
-
-    void dfs2(int i, int n, int p = -1) {
-        for(auto u : v[i]) {
-            if(u == p) continue;
-            res[u] = res[i] - counter[u] + n - counter[u];
-            dfs2(u, n, i);
-        }
-    }
-    
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        v.resize(n);
-        for(int i = 0; i < n - 1; i++) {
-            int a = edges[i][0];
-            int b = edges[i][1];
-            v[a].push_back(b);
-            v[b].push_back(a);
+        vector<int> dp(n),sz(n),ans(n);
+        
+        vector<int> g[n];
+        for(auto &edge : edges)
+        {
+            g[edge[0]].push_back(edge[1]); 
+            g[edge[1]].push_back(edge[0]);
         }
-        res.resize(n);
-        counter.resize(n);
-        dfs(0);
-        dfs2(0, n);
-        return res;
+        
+        function<void(int,int)> dfs = [&](int node,int prev){
+            dp[node] = 0;
+            sz[node] = 1;
+            
+            for(auto &child : g[node])
+            {
+                if(child == prev)
+                    continue;
+                dfs(child,node);
+                sz[node] += sz[child];
+                dp[node] += dp[child] + sz[child];
+            }
+        };
+        
+        function<void(int,int)> reroot = [&](int node,int src){
+            dp[src] -= dp[node] + sz[node];
+            sz[src] -= sz[node];
+            
+            dp[node] += dp[src] + sz[src];
+            sz[node] += sz[src];
+        };
+        
+        function<void(int,int)> dfs1 = [&](int node,int prev){
+            ans[node] = dp[node];
+            for(auto &child : g[node])
+            {
+                if(child == prev)
+                    continue;
+                reroot(child,node);
+                dfs1(child,node);
+                reroot(node,child);
+            }
+        };
+        
+        dfs(0,-1);
+        dfs1(0,-1);
+        
+        return ans;
     }
 };
